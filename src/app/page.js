615c0011aa1f2750950400cb3c7a4f2ee6ac153d
Pistol Pete <peterlawson@24marketing.co.uk>
2025-02-17 16:30:01 +0000

    added data

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,78 @@
+"use client";
+
+
 import Image from "next/image";
+import Lenis from 'lenis';
+import { useEffect } from "react";
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  useEffect(() => {
+    const lenis = new Lenis({
+      infinite: true,
+      syncTouch: true
+    });
+    
+    function onRaf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(onRaf);
+    }
+    
+    requestAnimationFrame(onRaf);
+    
+// repeat first six items by cloning them and appending them to the .grid
+const repeatItems = (parentEl, total = 0) => {
+  const items = [...parentEl.children];
+  for (let i = 0; i <= total - 1; ++i) {
+    var cln = items[i].cloneNode(true);
+    parentEl.appendChild(cln);
+  }
+};
+repeatItems(document.querySelector(".grid"), 20);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+
+    
+  }, []);
+
+
+  return (
+  
+     
+       <main> 
+          <div className="content">
+            <div className="grid text-[50px]">
+            <div className="grid__item"> 1</div>
+            <div className="grid__item">2</div>
+            <div className="grid__item"> 3</div>
+            <div className="grid__item"> 4</div>
+            <div className="grid__item"> 5</div>
+            <div className="grid__item">6</div>
+            <div className="grid__item"> 7</div>
+            <div className="grid__item"> 8</div>
+            <div className="grid__item"> 9</div>
+            <div className="grid__item">10</div>
+            <div className="grid__item"> 11</div>
+            <div className="grid__item"> 12</div>
+            <div className="grid__item"> 13</div>
+            <div className="grid__item">14</div>
+            <div className="grid__item"> 15</div>
+            <div className="grid__item"> 16</div>
+            <div className="grid__item"> 17</div>
+            <div className="grid__item">18</div>
+            <div className="grid__item"> 19</div>
+            <div className="grid__item"> 20</div>
+
+
+            </div>
+          </div>
+       </main>
+
+   
+
+  
+
+
+   
   );
 }
