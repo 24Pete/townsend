7fd13bef7b768ff62c06773c36b695a5aeb21ea1
Alex Birtwell <31503842+alexbirtwell@users.noreply.github.com>
2025-03-25 06:40:51 +0000

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -78,7 +78,7 @@ export default function RootLayout({ children }) {
           <div className="col-span-6 sm:col-span-3 sm:pl-[60px] hidden sm:block">
              <h4 className="font-extralight mb-[20px]">FIND US ON </h4> 
           <ul>
-            <li> <a href="" className="text-[18px] sm:text-[24px]font-extrabold">instagram</a></li>
+            <li> <a href="" className="text-[18px] sm:text-[24px] font-extrabold">instagram</a></li>
             <li> <a href="" className="text-[18px] sm:text-[24px] font-extrabold">facebook</a></li>
             <li> <a href="" className="text-[18px] sm:text-[24px] font-extrabold">x</a></li>
           </ul>
